6eea4a5c17a4ceb09f91b35f0e497e6641a29488
Eeshan2168 <eeshanmansuke@gmail.com>
2025-01-26 14:17:26 +0530

    Uploaded program to find the area of a square.
    Area of Square - C Program"
    
    A simple C program that calculates the area of a square. The user inputs the side length, and the program computes the area using the formula area = side * side.

---
+++ C programming language code no 15.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//write side of square to find the area of square
+int main() {
+int side;
+printf("enter side :");
+scanf("%d", & side) ;
+printf("The area of the square is :%d", side*side) ;
+return 0;
+} 
